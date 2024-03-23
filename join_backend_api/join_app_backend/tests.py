7269ce0201c20f9bb7ed66175ee2a39af3d7e657
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-03-23 14:24:40 +0100

    test case

--- join_backend_api/join_app_backend/tests.py
+++ join_backend_api/join_app_backend/tests.py
@@ -13,23 +13,48 @@ class LoginTest(TestCase):
         
         
 class ContactTest(TestCase):
-    def setUp(self):
+     def setUp(self):
         self.client = Client()
-        user = User.objects.create_user('test_user', password='test_password')
-        self.contact = ContactModel.objects.create(
-        name="Test Case",
-        email="Tesct@Case.at",
-        phonenumber="0123456",
-        short="TC",
-        iconColor= "#FFFFFF",
-        author= user
+        user = User.objects.create_user('test_user', password='test_user')
+        self.client.login(username='test_user', password='test_user')
+
+        # Create subtask
+        subtask = SubtaskModel.objects.create(titleFromSub="Test Subtask")
+
+        # Create contact
+        contact = ContactModel.objects.create(
+            name="Test Contact",
+            email="test@example.com",
+            phonenumber="0123456",
+            short="TC",
+            iconColor="#FFFFFF",
+            author=user
         )
+
+        # Create task
+        task = TaskModel.objects.create(
+            category="Media",
+            categoryColor="FFC701",
+            title="Test",
+            description="Test 12345",
+            priority="urgent",
+            section="taskCategoryInProgress",
+            date="2024-04-07",
+            author=user
+        )
+
+        # Add the subtask to the task
+        task.subtask.add(subtask)
+
+        # Add the contact to the task
+        task.assignedTo.add(contact)
+
     
-    def test_create_contact(self):
+     def test_create_contact(self):
         response = self.client.get('/tasks/')
         self.assertEqual(response.status_code, 200)
   
-    def test_get_contact(self):  
+     def test_get_contact(self):  
         response = self.client.get('/contacts/')
         self.assertEqual(response.status_code, 200) 
         
@@ -38,23 +63,37 @@ class TaskTest(TestCase):
         self.client = Client()
         user = User.objects.create_user('test_user', password='test_user')
         self.client.login(username='test_user', password='test_user')
-        task = TaskModel.objects.create(
-         title="Test",
-         description="Empty description",
-         priority= "to-do",
-         category="Max Mustermann",
-         subtask="nothing",
-         author= user
-        )
+        
+        # Create subtask
+        subtask = SubtaskModel.objects.create(titleFromSub="Test Subtask")
+        
+        # Create contact
         contact = ContactModel.objects.create(
-         name="Test Contact",
-         email="test@example.com",
-         phonenumber="0123456",
-         short="TC",
-         iconColor="#FFFFFF",
-         author=user
+            name="Test Contact",
+            email="test@example.com",
+            phonenumber="0123456",
+            short="TC",
+            iconColor="#FFFFFF",
+            author=user
         )
-        task.assigned_to.add(contact)
+        
+        # Create task
+        task = TaskModel.objects.create(
+            category="Media",
+            categoryColor="FFC701",
+            title="Test",
+            description="Test 12345",
+            priority="urgent",
+            section="taskCategoryInProgress",
+            date="2024-04-07",
+            author=user
+        )
+        
+        # Add the subtask to the task
+        task.subtask.add(subtask)
+        
+        # Add the contact to the task
+        task.assignedTo.add(contact)
         
     def test_create_task(self):
         response = self.client.get('/tasks/')
